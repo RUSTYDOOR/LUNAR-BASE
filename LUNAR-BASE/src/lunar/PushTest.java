d271856be689ec43403506dc1e557988ac7c8edd
LUXFERRE <rustydoor@gmail.com>
2016-05-12 11:04:14 +0900

    Update PushTest.java

--- LUNAR-BASE/src/lunar/PushTest.java
+++ LUNAR-BASE/src/lunar/PushTest.java
@@ -5,6 +5,7 @@ public class PushTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Push Test!!!");
+		System.out.println("Edit Server --> Push Test!!!");
 	}
 
 }
