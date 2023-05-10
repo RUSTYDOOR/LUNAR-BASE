b102e00d3be4a95ba0d93aa1a0e9c258d730317b
luxferre <rustydoor@gmail.com>
2023-05-10 17:09:21 +0900

    2023-05-10 : Commit

--- src/lunar/LunarMain.java
+++ src/lunar/LunarMain.java
@@ -189,7 +189,7 @@ public class LunarMain {
              */
             String[] arrIljin = new String[31];
             for ( int i=0; i < 31; i++ ) {
-                System.out.println(i + " : " + "197608" + String.format("%02d", i+1));
+                //- System.out.println(i + " : " + "197608" + String.format("%02d", i+1));
 
                 HashMap<?, ?> hmDayfortune = lunar.dayfortune("197608" + String.format("%02d", i+1));
                 arrIljin[i] = TextUtil.convertString( hmDayfortune.get("day") );
@@ -207,13 +207,27 @@ public class LunarMain {
             arrIljin[13] = TextUtil.convertString( hmDayfortune.get("day") );
             int iIndex   = ((int[])hmDayfortune.get("date"))[2];
 
-            for ( int i=2; i<31; i++ ) {
+            for ( int i=0; i < 31; i++ ) {
                 iIndex++;
                 if ( iIndex >= 60 )
                     iIndex -= 60;
                 arrIljin[i] = lunar.ganji_ref(iIndex, true);
             }
-            System.out.println("(Good Case) iljin : " + Arrays.toString(arrIljin));
+            System.out.println("(Good Case) iljin #Hanja : " + Arrays.toString(arrIljin));
+
+
+            hmDayfortune = lunar.dayfortune("19760813");
+            arrIljin     = new String[31];
+            arrIljin[13] = TextUtil.convertString( hmDayfortune.get("day") );
+            iIndex       = ((int[])hmDayfortune.get("date"))[2];
+
+            for ( int i=0; i < 31; i++ ) {
+                iIndex++;
+                if ( iIndex >= 60 )
+                    iIndex -= 60;
+                arrIljin[i] = lunar.ganji_ref(iIndex, false);
+            }
+            System.out.println("(Good Case) iljin #Hangul : " + Arrays.toString(arrIljin));
 
 
             /*
